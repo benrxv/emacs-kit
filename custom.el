84d8db397c5e24397b1d2db1f2f16a8ec2da296e
ben <benrxv@gmail.com>
2014-09-12 04:12:58 -0700

    Desktop path is dir emacs was started
    Set ibuffer by-vc-root
    
    Pony and django mode required
    
    Better uniquify

--- custom.el
+++ custom.el
@@ -25,16 +25,15 @@
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(js2-basic-offset 2)  
- '(js2-bounce-indent-p t)  
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(desktop-path (quote ("./")))
+ '(js2-basic-offset 2)
+ '(js2-bounce-indent-p t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -49,3 +48,29 @@
 
 (use-package ibuffer
   :bind ("C-x C-b" . ibuffer))
+
+;; Group the ibuffer list by vc-root
+(add-hook 'ibuffer-hook
+          (lambda ()
+            (ibuffer-vc-set-filter-groups-by-vc-root)
+            (ibuffer-do-sort-by-alphabetic)))
+
+
+(use-package uniquify
+             :config
+             (setq
+              uniquify-buffer-name-style 'reverse
+              uniquify-separator ":"
+              uniquify-after-kill-buffer-p t      ; rename after killing dupes
+              uniquify-ignore-buffers-re "^\\*"; don't muck with special buffers
+              ))
+
+(require 'django-mode)
+(require 'pony-mode)
+
+;; Change me to only do this in *scratch* buffer ;)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (when (string= (buffer-name) "*scratch*")
+              (animate-string ";; Emacs time!!"
+(/ (frame-height) 2)))))
